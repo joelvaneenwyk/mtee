23b79971f6b1166042d8db2970ee03be3d3ea6b7
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-01-22 11:07:51 -0800

    Run clang-format and add PCH
    Cleanup only with no functional changes. Due to PCH addition and some header order changes there could be some accidental functional changes, but they should be minimal.

--- helpers.cpp
+++ helpers.cpp
@@ -1,4 +1,5 @@
 #include "header.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -19,86 +20,86 @@
 
 PWSTR CreateFullPathW(PWSTR szPath)
 {
-	PWCHAR p = szPath;
+    PWCHAR p = szPath;
 
-	while(*p++)
-	{
-		if(*p == L'\\')
-		{
-			*p = L'\0';
-			CreateDirectoryW(szPath, NULL);
-			*p = L'\\';
-		}
-	}
-	return szPath;
+    while (*p++)
+    {
+        if (*p == L'\\')
+        {
+            *p = L'\0';
+            CreateDirectoryW(szPath, NULL);
+            *p = L'\\';
+        }
+    }
+    return szPath;
 }
 
 /*
 VOID MsgBox(LPCTSTR msg)
 {
-	MessageBox
-	(
-		NULL,
-		msg,
-		TEXT("MTEE BETA"),
-		MB_OK | MB_SETFOREGROUND
-	);
+    MessageBox
+    (
+        NULL,
+        msg,
+        TEXT("MTEE BETA"),
+        MB_OK | MB_SETFOREGROUND
+    );
 }
 */
 
 /*ID ShowFileType(HANDLE h)
 {
-	DWORD dwHndType;
-	dwHndType = GetFileType(h);
+    DWORD dwHndType;
+    dwHndType = GetFileType(h);
 
-	switch(dwHndType)
-	{
-	case FILE_TYPE_DISK: // stdin/stdout is redirected from/to disk: app.exe<in>out
-		MsgBox(TEXT("FILE_TYPE_DISK"));
-		break;
-	case FILE_TYPE_CHAR: // stdin from keyboard or nul/con/aux/comx, stdout to console or nul
-		MsgBox(TEXT("FILE_TYPE_CHAR"));
-		break;
-	case FILE_TYPE_PIPE: // stdin is from pipe prn/lptx, stdout piped to anything
-		MsgBox(TEXT("FILE_TYPE_PIPE"));
-		break;
-	default:
-		MsgBox(TEXT("FILE_TYPE_UNKNOWN"));
-		break;
-	}
+    switch(dwHndType)
+    {
+    case FILE_TYPE_DISK: // stdin/stdout is redirected from/to disk: app.exe<in>out
+        MsgBox(TEXT("FILE_TYPE_DISK"));
+        break;
+    case FILE_TYPE_CHAR: // stdin from keyboard or nul/con/aux/comx, stdout to console or nul
+        MsgBox(TEXT("FILE_TYPE_CHAR"));
+        break;
+    case FILE_TYPE_PIPE: // stdin is from pipe prn/lptx, stdout piped to anything
+        MsgBox(TEXT("FILE_TYPE_PIPE"));
+        break;
+    default:
+        MsgBox(TEXT("FILE_TYPE_UNKNOWN"));
+        break;
+    }
 }
 
 */
 /*DWORD GetWinVer(VOID)
 {
-	OSVERSIONINFO os;
-	os.dwOSVersionInfoSize = sizeof (OSVERSIONINFO);
-	DWORD myVersion;
+    OSVERSIONINFO os;
+    os.dwOSVersionInfoSize = sizeof (OSVERSIONINFO);
+    DWORD myVersion;
 
-	if(!GetVersionEx(&os)) return 0;
+    if(!GetVersionEx(&os)) return 0;
 
-	if(os.dwPlatformId != VER_PLATFORM_WIN32_NT) myVersion = VER_PRE_NT4;
-	else
-	{
-		switch(os.dwMajorVersion)
-		{
-			case 3:  myVersion = VER_PRE_NT4; break;
-			case 4:  myVersion = VER_NT4;     break;
-			default: myVersion = VER_2000;
-		}
-	}
+    if(os.dwPlatformId != VER_PLATFORM_WIN32_NT) myVersion = VER_PRE_NT4;
+    else
+    {
+        switch(os.dwMajorVersion)
+        {
+            case 3:  myVersion = VER_PRE_NT4; break;
+            case 4:  myVersion = VER_NT4;     break;
+            default: myVersion = VER_2000;
+        }
+    }
 
-	if(myVersion == VER_2000)
-	{
-		switch(os.dwMinorVersion)
-		{
-			case 0:  myVersion = VER_2000; break;
-			case 1:  myVersion = VER_XP;   break;
-			default: myVersion = VER_2003;
-		}
-	}
+    if(myVersion == VER_2000)
+    {
+        switch(os.dwMinorVersion)
+        {
+            case 0:  myVersion = VER_2000; break;
+            case 1:  myVersion = VER_XP;   break;
+            default: myVersion = VER_2003;
+        }
+    }
 
-	return myVersion;
+    return myVersion;
 }
 */
 /*
@@ -124,53 +125,54 @@ CE             3                                                           0
 //----------------------------------------------------------------------------
 DWORD GetFormattedDateTimeA(PCHAR lpBuf, BOOL bDate, BOOL bTime)
 {
-	SYSTEMTIME st;
-	DWORD dwSize = 0;
+    SYSTEMTIME st;
+    DWORD dwSize = 0;
 
-	GetLocalTime(&st);
+    GetLocalTime(&st);
 
-	if(bDate)
-	{
-		dwSize += wsprintfA(lpBuf, "%04u-%02u-%02u ", st.wYear, st.wMonth, st.wDay);
-	}
-	if(bTime)
-	{
-		dwSize += wsprintfA(lpBuf + dwSize, "%02u:%02u:%02u.%03u ", st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
-	}
-	return dwSize;
+    if (bDate)
+    {
+        dwSize += wsprintfA(lpBuf, "%04u-%02u-%02u ", st.wYear, st.wMonth, st.wDay);
+    }
+    if (bTime)
+    {
+        dwSize += wsprintfA(lpBuf + dwSize, "%02u:%02u:%02u.%03u ", st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
+    }
+    return dwSize;
 }
 
 //----------------------------------------------------------------------------
 DWORD GetFormattedDateTimeW(PWCHAR lpBuf, BOOL bDate, BOOL bTime)
 {
-	SYSTEMTIME st;
-	DWORD dwSize = 0;
+    SYSTEMTIME st;
+    DWORD dwSize = 0;
 
-	GetLocalTime(&st);
+    GetLocalTime(&st);
 
-	if(bDate)
-	{
-		dwSize += wsprintfW(lpBuf, L"%04u-%02u-%02u ", st.wYear, st.wMonth, st.wDay);
-	}
-	if(bTime)
-	{
-		dwSize += wsprintfW(lpBuf + dwSize, L"%02u:%02u:%02u.%03u ", st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
-	}
-	return dwSize; // return characters written, not bytes
+    if (bDate)
+    {
+        dwSize += wsprintfW(lpBuf, L"%04u-%02u-%02u ", st.wYear, st.wMonth, st.wDay);
+    }
+    if (bTime)
+    {
+        dwSize +=
+            wsprintfW(lpBuf + dwSize, L"%02u:%02u:%02u.%03u ", st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
+    }
+    return dwSize; // return characters written, not bytes
 }
 
 // determine whether the output is a console
 // this is hard. I first tried to use GetConsoleMode but it returns FALSE in case: mtee > con
 BOOL IsAnOutputConsoleDevice(HANDLE h)
 {
-	if (GetFileType(h) == FILE_TYPE_CHAR)
-	{
-		// CON, NUL, ...
-		DWORD dwBytesWritten;
-		if (WriteConsoleA(h, "", 0, &dwBytesWritten, NULL))
-			return TRUE;
-	}
-	return FALSE;
+    if (GetFileType(h) == FILE_TYPE_CHAR)
+    {
+        // CON, NUL, ...
+        DWORD dwBytesWritten;
+        if (WriteConsoleA(h, "", 0, &dwBytesWritten, NULL))
+            return TRUE;
+    }
+    return FALSE;
 }
 
 DWORD GetParentProcessId(VOID)
@@ -178,7 +180,8 @@ DWORD GetParentProcessId(VOID)
     DWORD ppid = 0, pid = GetCurrentProcessId();
 
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-    if (hSnapshot == INVALID_HANDLE_VALUE) Perror((DWORD)NULL);
+    if (hSnapshot == INVALID_HANDLE_VALUE)
+        Perror((DWORD)NULL);
 
     PROCESSENTRY32 pe = {0};
     pe.dwSize = sizeof(PROCESSENTRY32);
@@ -186,24 +189,27 @@ DWORD GetParentProcessId(VOID)
     //
     // find our PID in the process snapshot then lookup parent PID
     //
-    if(Process32First(hSnapshot, &pe)) {
-    	do {
-    		if (pe.th32ProcessID == pid) {
-    			ppid = pe.th32ParentProcessID;
-    			break;
+    if (Process32First(hSnapshot, &pe))
+    {
+        do
+        {
+            if (pe.th32ProcessID == pid)
+            {
+                ppid = pe.th32ParentProcessID;
+                break;
             }
-        } while(Process32Next(hSnapshot, &pe));
+        } while (Process32Next(hSnapshot, &pe));
     }
     CloseHandle(hSnapshot);
     return ppid;
 }
 
-static DWORD getActualNumberOfConsoleProcesses( VOID )
+static DWORD getActualNumberOfConsoleProcesses(VOID)
 {
     DWORD dummyProcessId;
     DWORD numberOfProcesses;
 
-    numberOfProcesses = GetConsoleProcessList( &dummyProcessId, 1 );
+    numberOfProcesses = GetConsoleProcessList(&dummyProcessId, 1);
 
     return numberOfProcesses;
 }
@@ -222,10 +228,10 @@ HANDLE GetPipedProcessHandle(VOID)
     //
 
     dwProcCount = getActualNumberOfConsoleProcesses();
-    lpdwProcessList = (DWORD*)malloc( dwProcCount * sizeof(DWORD) );
-    if( NULL != lpdwProcessList && dwProcCount > 0 )
+    lpdwProcessList = (DWORD *)malloc(dwProcCount * sizeof(DWORD));
+    if (NULL != lpdwProcessList && dwProcCount > 0)
     {
-        dwProcCount = GetConsoleProcessList( lpdwProcessList, dwProcCount );
+        dwProcCount = GetConsoleProcessList(lpdwProcessList, dwProcCount);
     }
     else
     {
@@ -245,11 +251,8 @@ HANDLE GetPipedProcessHandle(VOID)
     {
         if ((cpid != lpdwProcessList[dw]) && (ppid != lpdwProcessList[dw]))
         {
-            HANDLE Handle = OpenProcess(
-                                PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
-                                FALSE,
-                                lpdwProcessList[dw]);
-            if( INVALID_HANDLE_VALUE != Handle )
+            HANDLE Handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, lpdwProcessList[dw]);
+            if (INVALID_HANDLE_VALUE != Handle)
             {
                 hPipedProcess = Handle;
                 break;
@@ -257,12 +260,11 @@ HANDLE GetPipedProcessHandle(VOID)
         }
     }
 
-    free( lpdwProcessList );
+    free(lpdwProcessList);
     return hPipedProcess;
 }
 
-int FormatElapsedTime( LARGE_INTEGER* elapsedTime, PCHAR outBuf,
-                                                        const int outBufSize )
+int FormatElapsedTime(LARGE_INTEGER *elapsedTime, PCHAR outBuf, const int outBufSize)
 {
     int h = 0;
     int m = 0;
@@ -270,15 +272,12 @@ int FormatElapsedTime( LARGE_INTEGER* elapsedTime, PCHAR outBuf,
 
     float s = float(elapsedTime->QuadPart / 1000000);
     m = (int)(s / 60.0);
-    s = s - 60*m;
+    s = s - 60 * m;
 
-    h = (int)((float)m/60.0);
-    m = m - 60*h;
+    h = (int)((float)m / 60.0);
+    m = m - 60 * h;
 
-    len = snprintf( outBuf, outBufSize, "Elapsed time: %02dh%02dm%06.3fs\n", h, m, s);
+    len = snprintf(outBuf, outBufSize, "Elapsed time: %02dh%02dm%06.3fs\n", h, m, s);
 
     return len;
 }
-
-
-

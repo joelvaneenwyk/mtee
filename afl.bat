281c82a651b9e6c733377646c6f9d95433525c8c
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-01-27 15:28:03 -0800

    Re-enable `drrun` and cleanup run script

--- afl.bat
+++ afl.bat
@@ -13,47 +13,74 @@ if exist "%_root%\tests\output" rmdir /s /q "%_root%\tests\output"
 mkdir "%_root%\tests\output"
 
 set _msbuild=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe
-"%_msbuild%" /property:Configuration=Release "%_root%\workspace\mtee.sln"
-"%_msbuild%" /property:Configuration=Debug "%_root%\workspace\mtee.sln"
-
 set _mtee=%_root%\workspace\x64\Debug\mtee.exe
-
-set _dumpbin=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.29.30133\bin\Hostx86\x86\dumpbin.exe
-"%_dumpbin%" /all "%_mtee%" >mtee.txt
-
+set _dumpbin=C:\program files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.30.30705\bin\Hostx64\x64\dumpbin.exe
 set _afl_fuzz=%_root%\..\winafl\build\bin\Release\afl-fuzz.exe
 set _winafl=%_root%\..\winafl\build\bin\Release\winafl.dll
 set _dynamo=%_root%\..\DynamoRIO\9.0.0\bin64
 set _input=%~dp0tests\input
 set _output=%~dp0tests\output
+set _target_method=main
+set _target_module=%_mtee%
+set _target_module_name=mtee.exe
+set _target_offset=0x14EF0
+set _target_args=2
+set _target_iterations=100
 
-"%_dynamo%\drrun.exe" --help >afl.drrun.txt
+::
+:: Use MSBuild to compile Debug and Release binaries
+::
+
+call :Execute "%_msbuild%" /property:Configuration=Release "%_root%\workspace\mtee.sln"
+call :Execute "%_msbuild%" /property:Configuration=Debug "%_root%\workspace\mtee.sln"
+
+::
+:: `dumpbin` Dump all symbols so we can validate target offset
+::
+
+echo ##[cmd] "%_dumpbin%" /all "%_mtee%"
+"%_dumpbin%" /all "%_mtee%" >"%~dp0mtee.txt"
+
+::
+:: `drrun` Use this to validate that we can instrument the executable
+::
+
+echo ##[cmd] "%_dynamo%\drrun.exe" --help
+"%_dynamo%\drrun.exe" --help >"%~dp0afl.drrun.txt"
 set _dr_run=-stats -mem -verbose
 set _dr_run=!_dr_run! -c "%_winafl%"
 set _dr_run=!_dr_run! -debug
-set _dr_run=!_dr_run! -target_method main
-set _dr_run=!_dr_run! -target_module "%_mtee%"
-set _dr_run=!_dr_run! -target_offset 0x00014EF0
-set _dr_run=!_dr_run! -fuzz_iterations 100
-set _dr_run=!_dr_run! -nargs 2
-::echo ##[cmd] "%_dynamo%\drrun.exe" !_dr_run! -- "%_mtee%"
-::"%_dynamo%\drrun.exe" !_dr_run! -- "%_mtee%"
-::if errorlevel 1 (
-::    echo Failed.
-::    exit /b 1
-::)
+set _dr_run=!_dr_run! -target_method %_target_method%
+set _dr_run=!_dr_run! -target_module "%_target_module%"
+set _dr_run=!_dr_run! -target_offset %_target_offset%
+set _dr_run=!_dr_run! -fuzz_iterations %_target_iterations%
+set _dr_run=!_dr_run! -nargs %_target_args%
+call :Execute "%_dynamo%\drrun.exe" !_dr_run! -- "%_mtee%"
+if errorlevel 1 (
+    echo Failed.
+    exit /b 1
+)
+echo [SUCCESS] Instrumenting module succeeded.
+
+::
+:: afl_fuzz
+::
 
 set _afl_fuzz_args=-w "%_winafl%" -t 4000 -i "%_input%" -o "%_output%" -D "%_dynamo%"
-set _instrumentation=-debug
-set _instrumentation=!_instrumentation! -coverage_module mtee.exe
-set _instrumentation=!_instrumentation! -target_module mtee.exe
-::set _instrumentation=!_instrumentation! -target_offset 0x10d0
-set _instrumentation=!_instrumentation! -target_offset 0x00014EF0
-set _instrumentation=!_instrumentation! -target_method main
-set _instrumentation=!_instrumentation! -nargs 2
-set _instrumentation=!_instrumentation! -fuzz_iterations 100
-::set _instrumentation=!_instrumentation! -call_convention thiscall
-::set _instrumentation=!_instrumentation! -covtype edge
+set _afl_instrumentation=-debug
+set _afl_instrumentation=!_afl_instrumentation! -coverage_module "%_target_module%"
+set _afl_instrumentation=!_afl_instrumentation! -target_module "%_target_module%"
+set _afl_instrumentation=!_afl_instrumentation! -target_offset %_target_offset%
+set _afl_instrumentation=!_afl_instrumentation! -target_method %_target_method%
+set _afl_instrumentation=!_afl_instrumentation! -nargs %_target_args%
+set _afl_instrumentation=!_afl_instrumentation! -fuzz_iterations %_target_iterations%
+::set _afl_instrumentation=!_afl_instrumentation! -call_convention thiscall
+::set _afl_instrumentation=!_afl_instrumentation! -covtype edge
+call :Execute "%_afl_fuzz%" %_afl_fuzz_args% -- !_afl_instrumentation! -- "%_mtee%"
 
-echo ##[cmd] "%_afl_fuzz%" %_afl_fuzz_args% -- !_instrumentation! -- "%_mtee%"
-"%_afl_fuzz%" %_afl_fuzz_args% -- !_instrumentation! -- "%_mtee%"
+exit /b
+
+:Execute
+    echo ##[cmd] %*
+    %*
+exit /b

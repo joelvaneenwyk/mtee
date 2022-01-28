c3420ea3cfdd5b7169773ac44e4c922f20d0035a
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-01-27 20:12:08 -0800

    Cleanup root folder

--- mtee.bat
+++ mtee.bat
@@ -2,7 +2,22 @@
 
 setlocal EnableDelayedExpansion
 
-set _root=%~dp0
-set _mtee=%_root%\workspace\x64\Debug\mtee.exe
+call :RunTee "%~dp0"
+exit /b
 
-"%_mtee%" %*
+:RunTee
+    setlocal EnableDelayedExpansion
+    set _root=%~dp1
+    if %_root:~-1%==\ set _root=%_root:~0,-1%
+
+    set _mtee_config=Debug
+    set _mtee=%_root%\workspace\x64\%_mtee_config%\mtee.exe
+
+    set _msbuild=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe
+
+    if not exist "%_mtee%" (
+        "%_msbuild%" /property:Configuration=%_mtee_config% "%_root%\workspace\mtee.sln"
+    )
+
+    "%_mtee%" %*
+exit /b

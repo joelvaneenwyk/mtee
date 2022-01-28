46cbf662ba4d27cfb6be6bf621659580ac2024d4
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-01-27 20:06:42 -0800

    By specifying only module name we now get further

--- tests/winafl.bat
+++ tests/winafl.bat
@@ -11,24 +11,29 @@ exit /b
     if %_root:~-1%==\ set _root=%_root:~0,-1%
     echo Root: !_root!
 
-    del "%_root%\afl.mtee.exe.*"
-    del "%_root%\afl.*.txt"
-    del "%_root%\mtee.txt"
+    set _tests=%_root%\tests
+    del "%_tests%\afl.mtee.exe.*"
+    del "%_tests%\afl.*.txt"
+    del "%_tests%\mtee.txt"
 
-    if exist "%_root%\tests\output" rmdir /s /q "%_root%\tests\output"
-    mkdir "%_root%\tests\output"
+    if exist "%_tests%\output" rmdir /s /q "%_tests%\output"
+    mkdir "%_tests%\output"
 
-    set _mtee=%_root%\workspace\x64\Debug\mtee.exe
+    set _mtee_config=Debug
+    set _mtee=%_root%\workspace\x64\%_mtee_config%\mtee.exe
 
-    set _afl_fuzz=%_root%\tests\winafl\build\bin\Release\afl-fuzz.exe
-    set _winafl=%_root%\tests\winafl\build\bin\Release\winafl.dll
-    set _dynamo=%_root%\tests\dynamorio\build\bin64
+    set _winafl_root=%_tests%\winafl
+    set _afl_fuzz=%_winafl_root%\build\bin\Release\afl-fuzz.exe
+    set _winafl=%_winafl_root%\build\bin\Release\winafl.dll
+
+    set _dynamorio_root=%_tests%\dynamorio
+    set _dynamorio_bin=%_dynamorio_root%\build\bin64
 
     set _msbuild=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe
     set _dumpbin=C:\program files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.30.30705\bin\Hostx64\x64\dumpbin.exe
 
-    set _input=%_root%\tests\input
-    set _output=%_root%\tests\output
+    set _input=%_tests%\input
+    set _output=%_tests%\output
     set _target_method=main
     set _target_module=%_mtee%
     set _target_module_name=mtee.exe
@@ -37,11 +42,10 @@ exit /b
     set _target_iterations=100
 
     ::
-    :: Use MSBuild to compile Debug and Release binaries
+    :: Use MSBuild to compile `mtee` for the configuration we are testing
     ::
 
-    call :Execute "%_msbuild%" /property:Configuration=Release "%_root%\workspace\mtee.sln"
-    call :Execute "%_msbuild%" /property:Configuration=Debug "%_root%\workspace\mtee.sln"
+    call :Execute "%_msbuild%" /property:Configuration=%_mtee_config% "%_root%\workspace\mtee.sln"
 
     ::
     :: Build `dynamorio` from source.
@@ -49,19 +53,21 @@ exit /b
 
     set "_dynamorio_config=RelWithDebInfo"
     set "_dynamorio_settings=-DCMAKE_BUILD_TYPE=%_dynamorio_config%"
-    if not exist "%_root%\tests\dynamorio\build" mkdir "%_root%\tests\dynamorio"
+    if not exist "%_dynamorio_root%\build" mkdir "%_dynamorio_root%\build"
 
-    cmake -S "%_root%\tests\dynamorio" -B "%_root%\tests\dynamorio\build" -G "Visual Studio 16 2019" -A x64 %_dynamorio_settings%
-    if errorlevel 1 (
-        echo Failed to generate `dynamorio` project files.
-        exit /b 1
-    )
+    if exist "%_dynamorio_bin%\drrun.exe" goto:$SkipDynamorioBuild
+        cmake -S "%_dynamorio_root%" -B "%_dynamorio_root%\build" -G "Visual Studio 16 2019" -A x64 %_dynamorio_settings%
+        if errorlevel 1 (
+            echo Failed to generate `dynamorio` project files.
+            exit /b 1
+        )
 
-    cmake --build "%_root%\tests\dynamorio\build" --config %_dynamorio_config%
-    if errorlevel 1 (
-        echo Failed to build `dynamorio` from source.
-        exit /b 1
-    )
+        cmake --build "%_dynamorio_root%\build" --config %_dynamorio_config%
+        if errorlevel 1 (
+            echo Failed to build `dynamorio` from source.
+            exit /b 1
+        )
+    :$SkipDynamorioBuild
 
     ::
     :: Build `winafl` from source
@@ -70,40 +76,42 @@ exit /b
     set "_winafl_config=Release"
     set "_winafl_settings=-DCMAKE_BUILD_TYPE=%_winafl_config%  -DUSE_COLOR=1 -DUSE_DRSYMS=1 -DDynamoRIO_DIR=%_root%/tests/dynamorio/build/cmake"
 
-    cmake -S "%_root%\tests\winafl" -B "%_root%\tests\winafl\build" -G "Visual Studio 16 2019" -A x64 %_winafl_settings% -DCMAKE_INSTALL_PREFIX="%_root%\tests\winafl\bin64"
-    if errorlevel 1 (
-        echo Failed to generate `winafl` project files.
-        exit /b 1
-    )
+    if exist "%_afl_fuzz%" goto:$SkipWinAflBuild
+        cmake -S "%_winafl_root%" -B "%_winafl_root%\build" -G "Visual Studio 16 2019" -A x64 %_winafl_settings% -DCMAKE_INSTALL_PREFIX="%_winafl_root%\bin64"
+        if errorlevel 1 (
+            echo Failed to generate `winafl` project files.
+            exit /b 1
+        )
 
-    cmake --build "%_root%\tests\winafl\build" --config %_winafl_config%
-    if errorlevel 1 (
-        echo Failed to build `winafl` from source.
-        exit /b 1
-    )
+        cmake --build "%_winafl_root%\build" --config %_winafl_config%
+        if errorlevel 1 (
+            echo Failed to build `winafl` from source.
+            exit /b 1
+        )
+    :$SkipWinAflBuild
 
     ::
     :: `dumpbin` Dump all symbols so we can validate target offset
     ::
 
     echo ##[cmd] "%_dumpbin%" /all "%_mtee%"
-    "%_dumpbin%" /all "%_mtee%" >"%~dp0mtee.txt"
+    "%_dumpbin%" /all "%_mtee%" >"%_tests%\mtee.txt"
 
     ::
     :: `drrun` Use this to validate that we can instrument the executable
     ::
 
-    echo ##[cmd] "%_dynamo%\drrun.exe" --help
-    "%_dynamo%\drrun.exe" --help >"%~dp0afl.drrun.txt"
+    echo ##[cmd] "%_dynamorio_bin%\drrun.exe" --help
+    "%_dynamorio_bin%\drrun.exe" --help >"%_tests%\afl.drrun.txt"
     set _dr_run=-stats -mem -verbose
     set _dr_run=!_dr_run! -c "%_winafl%"
     set _dr_run=!_dr_run! -debug
     set _dr_run=!_dr_run! -target_method %_target_method%
-    set _dr_run=!_dr_run! -target_module "%_target_module%"
-    set _dr_run=!_dr_run! -target_offset %_target_offset%
+    set _dr_run=!_dr_run! -target_module "%_target_module_name%"
+    ::set _dr_run=!_dr_run! -target_offset %_target_offset%
     set _dr_run=!_dr_run! -fuzz_iterations %_target_iterations%
     set _dr_run=!_dr_run! -nargs %_target_args%
-    call :Execute "%_dynamo%\drrun.exe" !_dr_run! -- "%_mtee%"
+    call :Execute "%_dynamorio_bin%\drrun.exe" !_dr_run! -- "%_mtee%"
     if errorlevel 1 (
         echo Failed.
         exit /b 1
@@ -114,14 +122,14 @@ exit /b
     :: afl_fuzz
     ::
 
-    set _afl_fuzz_args=-w "%_winafl%" -t 4000 -i "%_input%" -o "%_output%" -D "%_dynamo%"
+    set _afl_fuzz_args=-w "%_winafl%" -t 4000 -i "%_input%" -o "%_output%" -D "%_dynamorio_bin%"
     set _afl_instrumentation=-debug
-    set _afl_instrumentation=!_afl_instrumentation! -coverage_module "%_target_module%"
-    set _afl_instrumentation=!_afl_instrumentation! -target_module "%_target_module%"
-    set _afl_instrumentation=!_afl_instrumentation! -target_offset %_target_offset%
+    set _afl_instrumentation=!_afl_instrumentation! -coverage_module "%_target_module_name%"
+    set _afl_instrumentation=!_afl_instrumentation! -target_module "%_target_module_name%"
     set _afl_instrumentation=!_afl_instrumentation! -target_method %_target_method%
     set _afl_instrumentation=!_afl_instrumentation! -nargs %_target_args%
     set _afl_instrumentation=!_afl_instrumentation! -fuzz_iterations %_target_iterations%
+    ::set _afl_instrumentation=!_afl_instrumentation! -target_offset %_target_offset%
     ::set _afl_instrumentation=!_afl_instrumentation! -call_convention thiscall
     ::set _afl_instrumentation=!_afl_instrumentation! -covtype edge
     call :Execute "%_afl_fuzz%" %_afl_fuzz_args% -- !_afl_instrumentation! -- "%_mtee%"

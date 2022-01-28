4565f67ebab938ed658aac795dcb36ac93cbbaa3
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2022-01-27 19:26:03 -0800

    Add winafl directly as submodule and compile as part of test

--- tests/winafl.bat
+++ tests/winafl.bat
@@ -2,7 +2,7 @@
 
 setlocal EnableDelayedExpansion
 
-set _root=%~dp0
+set _root=%~dp0..\
 if %_root:~-1%==\ set _root=%_root:~0,-1%
 
 del "%_root%\afl.mtee.exe.*"
@@ -12,14 +12,17 @@ del "%_root%\mtee.txt"
 if exist "%_root%\tests\output" rmdir /s /q "%_root%\tests\output"
 mkdir "%_root%\tests\output"
 
-set _msbuild=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe
 set _mtee=%_root%\workspace\x64\Debug\mtee.exe
+
+set _afl_fuzz=%_root%\tests\winafl\build\bin\Release\afl-fuzz.exe
+set _winafl=%_root%\tests\winafl\build\bin\Release\winafl.dll
+set _dynamo=%_root%\tests\DynamoRIO\9.0.0\bin64
+
+set _msbuild=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MSBuild.exe
 set _dumpbin=C:\program files\Microsoft Visual Studio\2022\Enterprise\VC\Tools\MSVC\14.30.30705\bin\Hostx64\x64\dumpbin.exe
-set _afl_fuzz=%_root%\..\winafl\build\bin\Release\afl-fuzz.exe
-set _winafl=%_root%\..\winafl\build\bin\Release\winafl.dll
-set _dynamo=%_root%\..\DynamoRIO\9.0.0\bin64
-set _input=%~dp0tests\input
-set _output=%~dp0tests\output
+
+set _input=%_root%\tests\input
+set _output=%_root%\tests\output
 set _target_method=main
 set _target_module=%_mtee%
 set _target_module_name=mtee.exe
@@ -33,6 +36,19 @@ set _target_iterations=100
 
 call :Execute "%_msbuild%" /property:Configuration=Release "%_root%\workspace\mtee.sln"
 call :Execute "%_msbuild%" /property:Configuration=Debug "%_root%\workspace\mtee.sln"
+
+::
+:: Build `winafl` from source
+::
+
+set "config=Release"
+set "settings=-DCMAKE_BUILD_TYPE=%config%  -DUSE_COLOR=1 -DUSE_DRSYMS=1 -DDynamoRIO_DIR=%_root%/tests/DynamoRIO/9.0.0/cmake"
+cmake -S "%_root%\tests\winafl" -B "%_root%\tests\winafl\build" -G "Visual Studio 16 2019" -A x64 %settings% -DCMAKE_INSTALL_PREFIX="%_root%\tests\winafl\bin64"
+cmake --build "%_root%\tests\winafl\build" --config %config%
+if errorlevel 1 (
+    echo Failed to build `winafl` from source.
+    exit /b 1
+)
 
 ::
 :: `dumpbin` Dump all symbols so we can validate target offset
